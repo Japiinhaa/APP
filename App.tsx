4a26a466a07b4d654c1f9b8be5f1b9606d6e8fb1
japiinhaa <vitorgoncalves964@gmail.com>
2024-02-22 16:20:04 -0300

    update2

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import Recu from "./screens/Recu";
+
+function App() {  
+  return (
+    <View>
+      <Recu />
+    </View>
+  );
+}
+
+export default App;
---
+++ screens/Recu.tsx
@@ -0,0 +1,33 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import Logo from "../components/Logo";
+import Input from "../components/Input";
+import Botao from "../components/Botao";
+
+function Recu() {
+
+    const styles = StyleSheet.create({
+        text : {
+            fontSize: 16,
+            color: 'black',
+            alignContent: 'center',
+            textAlign: 'center',
+            marginBottom: 20,
+            fontWeight: 'bold', 
+        },
+    });
+
+    return (
+        <View>
+            <StatusBar style="auto" />
+            <Logo text="Recuperação de Senha" color="#011E83" />
+            <Text style={styles.text}>Insira seu e-mail para recuperar a senha!</Text>
+            <Input label="Email:" value="" setValue="" placeholder="Insira seu e-mail:" />
+            <Botao text="Enviar" color="#101010" />
+        </View>
+    );
+}
+
+
+export default Recu;
