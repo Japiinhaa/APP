6ba00104d401df3da20c7218fd691c47224df346
japiinhaa <vitorgoncalves964@gmail.com>
2024-02-27 15:16:06 -0300

    finalizacao da page inventario

--- components/Itens.tsx
+++ components/Itens.tsx
@@ -1,38 +1,37 @@
 import React from "react";
 import { Image, Text, View, StyleSheet } from "react-native";
 
-const itens = ({id, item }) => {
-    const styles = StyleSheet.create({
-    
-        display: {
-            height: 66,
-            width: 360,
-            borderBottomWidth: 1,
-            borderColor: '#F5F5F5',
-            paddingHorizontal: 20,
-            paddingVertical: 10,
-            gap: 10,
-        },
-        text : {
-            fontSize: 16,
-            fontWeight: '600',
-            lineHeight: 19,
-            color: 'black',
-        },
-        item: {
-            fontSize: 14,
-            fontWeight: '400',
-            lineHeight: 16,
-            color: 'lightgray',
-        },
-    });
-
-    return (
+const Itens = ({id, item }) => {
+    return(
         <View style={styles.display}>
             <Text style={styles.text}>{id}</Text>
             <Text style={styles.item}>{item}</Text>
         </View>
     );
-}
+};
 
-export default itens;
+export default Itens;
+
+const styles = StyleSheet.create({
+    display: {
+        borderBottomColor: '#f5f5f5',
+        borderBottomWidth: 2,
+    },
+    text : {
+        fontSize: 20,
+        fontWeight: '500',
+        color: 'black',
+        alignContent: 'center',
+        marginTop: 20,
+
+        marginLeft: 25,
+    },
+    item: {
+        fontSize: 10,
+        fontWeight: '500',
+        color: 'lightgray',
+        alignContent: 'center',
+        marginLeft: 25,
+        marginBottom: 20,
+    },
+});
