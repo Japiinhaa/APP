9b9fcb0fb908d13abb2a2fe4d0db116cfa79b86b
Vitor Gonçalves <133777140+Japiinhaa@users.noreply.github.com>
2024-04-18 07:20:16 -0300

    Reajuste na page de Cadastro

--- app/Cadastro.tsx
+++ app/Cadastro.tsx
@@ -1,44 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity , Linking, ScrollView } from 'react-native';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
-import React,{ useState } from 'react';
-import Logo from '../components/Logo';
-import Botao from '../components/Botao';
-import Input from '../components/Input';
+import React from 'react';
+import { View, useColorScheme } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { Link } from 'expo-router';
+import Logo from '../components/logo';
+import InputField from '../components/input';
+import Botao from '../components/botao';
+import useTheme from '../temas/Temas';
 
-function cadastro () {
+export default function Cadastro() {
+  const cores = useTheme();
   return (
+
     <KeyboardAwareScrollView
       resetScrollToCoords={{ x: 0, y: 0 }}
       contentContainerStyle={{ flexGrow: 1 }}
       scrollEnabled={false}
     >
-    <View>
-      <ScrollView>
-        <View>
-            <Logo text="Cadastre-se" color="red" />
-        </View>
-        <View>
-            <Input label="Nome" placeholder="Digite seu nome" secureTextEntry={false} />
-            <Input label="Sobrenome" placeholder="Digite seu Sobrenome" secureTextEntry={false} />
-            <Input label="Email" placeholder="Digite seu Email" secureTextEntry={false} />
-            <Input label="Telefone" placeholder="Digite seu Telefone" secureTextEntry={false} />
-            <Input label="Senha" placeholder="Digite sua Senha" secureTextEntry={true} />
-            <Input label="Confirme sua Senha" placeholder="Confirme sua Senha" secureTextEntry={false} />
-            
-            <Link href="/Inventarios" asChild>
-              <Botao text="Cadastrar" color="black" />
-            </Link>
-
-        </View>
-        <StatusBar style="auto" />
-        </ScrollView>
-    </View>
+      <View style={
+        {backgroundColor: cores.bgPrimary, height: '100%'}
+      }>
+        <Logo text="Cadastre-se" color={cores.bgPrimary} />
+        <InputField label="Nome" placeholder="Digite seu nome" secureTextEntry={false} />
+        <InputField label="Sobrenome" placeholder="Digite seu Sobrenome" secureTextEntry={false} />
+        <InputField label="Email" placeholder="Digite seu Email" secureTextEntry={false} />
+        <InputField label="Telefone" placeholder="Digite seu Telefone" secureTextEntry={false} />
+        <InputField label="Senha" placeholder="Digite sua Senha" secureTextEntry={true} />
+        <InputField label="Confirme sua Senha" placeholder="Confirme sua Senha" secureTextEntry={true}/>
+        <Botao text="Cadastrar" color={cores.bgSecondary  } href='/index' />
+      </View>
     </KeyboardAwareScrollView>
   );
 }
-
-export default cadastro;
