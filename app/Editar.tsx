af86c795dbb0f2fb01c3820ec2a8aa64d7a2e019
Japiinhaa <vitorgoncalves964@gmail.com>
2024-04-23 13:26:12 -0300

    atualização da page index, na componente Header e correção de tema light do app

--- app/Editar.tsx
+++ app/Editar.tsx
@@ -1,31 +1,27 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Header from "../components/Header";
-import Inputcad from "../components/Inputcad";
+import InventoryInput from "../components/Inputcad";
 import Bottom from "../components/Bottom";
-import { AntDesign } from '@expo/vector-icons';
-
 export default function AddItens() {
-        
-    const styles = StyleSheet.create({
-    
-        paddingtop: {
+        const styles = StyleSheet.create({
+    paddingtop: {
         paddingTop: 60,
     }});
 
     return (
         <View>
-            <View>
-                    <Header text="Cadastro de itens" color="red" colortxt="white" icon1={<AntDesign name="arrowleft" size={24} color="white" />} icon2={<AntDesign name="checkcircle" size={24} color="white" />}/>
-                <View style={styles.paddingtop}>
-                    <Inputcad label='N inventario'/>
-                    <Inputcad label='Descrição' />
-                    <Inputcad label='Local'/>
-                    <Inputcad label='Responsavel'/>
-                    <Inputcad label='Data de registro'/>
-                </View>
-                    <Bottom color="ffff" coloricons= "#595959" bottom={0} position="fixed" />
-            </View>
-        </View>
+<View>
+    <Header cor="#ff0000" texto="Adicionar Itens" />
+    <View style={styles.paddingtop}>
+    <InventoryInput label='N inventario'/>
+    <InventoryInput label='Descrição' />
+    <InventoryInput label='Local'/>
+    <InventoryInput label='Responsavel'/>
+    <InventoryInput label='Data de registro'/>
+    </View>
+    <Bottom color="ffff" coloricons= "#595959" bottom={0} position="fixed" />
+    </View>
+</View>
     );
 } 
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import {AntDesign } from '@expo/vector-icons';
 import { Link } from 'expo-router';
 
-export default function Header({ text, color, colortxt, icon1, icon2 }) {
+export default function Header({ texto, cor, colortxt, icon1, icon2 }) {
     const styles = StyleSheet.create({
         displaytop: {
             padding: 20,
@@ -11,7 +11,7 @@ export default function Header({ text, color, colortxt, icon1, icon2 }) {
             width: "100%",
             flexDirection: "row",
             justifyContent: "space-between",
-            backgroundColor: color
+            backgroundColor: cor
         }
     });
 
@@ -19,7 +19,7 @@ export default function Header({ text, color, colortxt, icon1, icon2 }) {
         <View>
             <View style={styles.displaytop}>
                 <AntDesign name={icon1} size={24} color={colortxt} />
-                <Text style={{ color: colortxt }}>{text}</Text>
+                <Text style={{ color: colortxt }}>{texto}</Text>
                 <Link href="/InfoItens" asChild>
                     <TouchableOpacity>
                         <AntDesign name={icon2} size={24} color={colortxt} />
