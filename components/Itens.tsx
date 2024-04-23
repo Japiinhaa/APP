f52dad3cf42281def6cd3d1018575381c27b0d2f
Japiinhaa <vitorgoncalves964@gmail.com>
2024-04-23 13:11:34 -0300

    correcao do componente itens para o modo dark e criação de itens.tsx e index.tsx na home

--- components/Itens.tsx
+++ components/Itens.tsx
@@ -1,40 +1,33 @@
 import React from "react";
-import { Image, Text, View, StyleSheet } from "react-native";
+import { Image, Text, View, StyleSheet,  } from "react-native";
+import useTheme from "../temas/Temas";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Link } from "expo-router";
 
 type ItensProps = {
-    id: string,
-    item: string,
+id: string,
+item: string,
+href : string,
 }
 
-const Itens = ({id, item }: ItensProps) => {
-    return(
-        <View style={styles.display}>
-            <Text style={styles.text}>{id}</Text>
-            <Text style={styles.item}>{item}</Text>
-        </View>
-    );
-};
-
-export default Itens;
-
-const styles = StyleSheet.create({
+const Itens = ({id, item, href}: ItensProps) => {
+    const cores = useTheme();
+    const styles = StyleSheet.create({
     display: {
-        borderTopColor: '#f5f5f5',
-        borderTopWidth: 2,
-        borderBottomColor: '#f5f5f5',
-        borderBottomWidth: 1,
+        borderBottomColor: cores.bgprimaryvariant,
+        borderBottomWidth: 2,
     },
     text : {
         fontSize: 20,
         fontWeight: '500',
-        color: 'black',
+        color: cores.textColor,
         alignContent: 'center',
         marginTop: 20,
 
         marginLeft: 25,
     },
     item: {
-        fontSize: 10,
+        fontSize: 15,
         fontWeight: '500',
         color: 'lightgray',
         alignContent: 'center',
@@ -42,3 +35,14 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
 });
+    return(
+    <Link href={href} asChild>
+        <TouchableOpacity style={styles.display}>
+            <Text style={styles.text}>{id}</Text>
+            <Text style={styles.item}>{item}</Text>
+        </TouchableOpacity>
+    </Link>
+    );
+};
+
+export default Itens;
