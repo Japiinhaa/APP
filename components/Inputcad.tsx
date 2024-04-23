c9d76748525536f21328d9651f2f5b528527e224
Vitor Gonçalves <133777140+Japiinhaa@users.noreply.github.com>
2024-04-23 13:03:52 -0300

    Correção relacionada ao tema light padrão das pages.

--- components/Inputcad.tsx
+++ components/Inputcad.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { View, Text, TextInput, StyleSheet, useColorScheme } from 'react-native';
 import useTheme from '../temas/Temas';
 
-export default function Inputcad({label}) {
-    const [hover, sethover] = React.useState('white');
+export default function InventoryInput({label}) {
+    const [hover, sethover] = React.useState('#5D5C5C');
     const cores = useTheme();
     const styles = StyleSheet.create({
     inventoryInput: {
@@ -37,7 +37,7 @@ export default function Inputcad({label}) {
             <TextInput style={styles.input} 
             
             onFocus={() => sethover("#f39200")}
-            onBlur={() => sethover("#fff")}/>
+            onBlur={() => sethover("#5D5C5C")}/>
             <Text style={[styles.label,]}>{label}</Text>
         </View>
     );
